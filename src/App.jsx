1dc2a0b6e019c09d8566b8004d4e66e475a8e7a0
pradeepjploftdev <pradeep_rawat@jploft.in>
2025-10-07 11:29:02 +0530

    First Commit: Rect + Vite + tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-red fw-bold text-xl bg-red-50'>AI Finance Manager</h1>
+    </>
+  )
+}
+
+export default App
